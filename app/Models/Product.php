7d5c772a66339a4596af216b9a9e0b62321dc436
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-03 00:57:00 -0700

    :bug: Add fillable properties to Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category_id',
+        'brand_id',
+        'photo_file_name',
+    ];
 }
