cd7e079d93f5efc6878f7b5715b6caf7e2f314a0
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-03 23:19:01 -0700

    :sparkles: Add created_by and updated_by columns to brand table

--- database/migrations/2023_12_03_015443_create_brands_table.php
+++ database/migrations/2023_12_03_015443_create_brands_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -16,6 +17,8 @@ return new class extends Migration
             $table->timestamps();
             $table->string('name', 255);
             $table->string('logo_file_name')->nullable();
+            $table->foreignIdFor(User::class, 'created_by')->nullable();
+            $table->foreignIdFor(User::class, 'updated_by')->nullable();
         });
     }
 
