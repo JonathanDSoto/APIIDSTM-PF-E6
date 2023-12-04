1839ba83500fa92c1d9e30a1f96fcda01f57b8a4
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2023-12-03 23:03:32 -0700

    :recycle: Remove unnecessary columns from brands table

--- database/migrations/2023_12_03_015443_create_brands_table.php
+++ database/migrations/2023_12_03_015443_create_brands_table.php
@@ -15,9 +15,6 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name', 255);
-            $table->string('website_1', 255)->nullable();
-            $table->string('website_2', 255)->nullable();
-            $table->string('email', 255)->nullable()->unique();
             $table->string('logo_file_name')->nullable();
         });
     }
